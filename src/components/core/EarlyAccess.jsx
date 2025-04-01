ee640092a2307419eb99de2116a9b655dab55f35
pacheko-ke <patrickoluochex@gmail.com>
2025-04-01 19:18:55 +0300

    delay early access form

--- src/components/core/EarlyAccess.jsx
+++ src/components/core/EarlyAccess.jsx
@@ -3,7 +3,7 @@ import { motion } from 'motion/react'
 
 export default function Newsletter(props) {
   return (
-    <motion.div className='flex gap-4 flex-col mx-10 z-20 -mt-24  md:w-1/4 md:gap-8 align-right bg-gray-800 px-6 py-4 text-white' initial={{ y: 500 }} animate={{ y: 0 }} transition={{ duration: 2 }}>
+    <motion.div className='flex gap-4 flex-col mx-10 z-20 -mt-24  md:w-1/4 md:gap-8 align-right bg-gray-800 px-6 py-4 text-white' initial={{ y: 500 }} animate={{ y: 0 }} transition={{ duration: 2,delay:2 }}>
       <p className="text-sm">Sign up to get early access to Keja. We will mail you once the service launch.  </p>
       <input type="email" placeholder='Enter email address' className='border pl-2 py-2 rounded-md invalid:border-red-500 invalid:border-2 text-white' />
       <button className='bg-[#F1F252] py-2 rounded-md text-black'>Get Early Access</button>
