92348c02089ff8cf9bd2d397c2cdb5cd29acf15f
pacheko-ke <patrickoluochex@gmail.com>
2025-04-01 19:41:56 +0300

    reduce horizontal margin on early access form

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,7 +19,7 @@ export default function Home() {
     }
 
     return (
-        <div className='flex flex-col '>
+        <div className='flex flex-col relative '>
             <div className='flex flex-col bg-[#1c1b18] text-white text-left px-4 gap-2 py-4 pt-20'>
                 <motion.h1 initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1, }} className=' text-3xl font-bold'>Property management, revenue collection and billing under one roof.</motion.h1>
                 <motion.p initial={{ x: 500 }} animate={{ x: 0 }} transition={{ duration: 0.5 }}>Grow recurring revenue, and reduce costs with a single unified platform for payments, subscriptions, invoicing, accounting, tax, and more.</motion.p>
