387dfaccdbd6f92f31cb8f308a0ca35f6657af27
Pacheko <patrickoluochex@gmail.com>
2025-04-01 16:53:02 +0300

    update brand name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ export default function Navbar() {
       {/* mobile nav */}
       <ul className={navOpen ? 'text-white lg:hidden h-full bg-[#1c1b18] w-3/4 text-5xl gap-2 flex flex-col fixed top-0' : 'text-white hidden h-full bg-red-500 w-3/4 text-5xl gap-2  flex-col fixed top-0'}>
         <h1 className='absolute w-full text-right pr-6 text-4xl' onClick={openNav}>x</h1>
-        <div className=' text-white top-0 py-2  italic mb-10 mt-10'>Keja<span className='text-[#F1F252]'>.</span></div>
+        <div className=' text-white top-0 py-2   mb-10 mt-10'>realtors<span className='text-[#F1F252]'>.</span></div>
 
         <li>Pricing</li>
         {/* <div className='relative'>
@@ -31,7 +31,7 @@ export default function Navbar() {
         <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10 text-white" onClick={openNav}>
           <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
         </svg>
-        <div className={!navOpen ? ' text-white top-0 py-2 text-2xl italic ' : 'hidden'}>Keja<span className='text-[#F1F252]'>.</span></div>
+        <div className={!navOpen ? ' text-white top-0 py-2 text-2xl italic ' : 'hidden'}>realtors<span className='text-[#F1F252]'>.</span></div>
 
       </div>
     </div>
