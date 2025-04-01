1a940614a688fc75901b4467231f8b26413068e5
Pacheko <patrickoluochex@gmail.com>
2025-04-01 15:47:20 +0300

    add tailwind

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -6,10 +6,10 @@ import Features from './Features'
 export default function Landing() {
   return (
     <>
-      <h1>Hello world</h1>
-      {/* <Navbar /> */}
-      {/* <Home /> */}
-      {/* <Features /> */}
+
+      <Navbar />
+      <Home />
+      <Features />
     </>
   )
 }
