09b3464f9c6502b5a185aaf7d01d17893acf8d16
pacheko-ke <patrickoluochex@gmail.com>
2025-04-01 21:04:57 +0300

    add padding to mobile nav

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -22,7 +22,7 @@ export default function Home() {
     return (
         <div className='flex flex-col relative '>
             <div className='flex bg-[#1c1b18] md:h-[80vh]'>
-                <div className='flex flex-col  text-white text-left px-4 gap-2 py-4 lg:pt-20 md:basis-1/2 md:gap-8 mt-10'>
+                <div className='flex flex-col  text-white text-left px-4 gap-2 py-4 lg:pt-20 pt-10 md:basis-1/2 md:gap-8 mt-10'>
                     <motion.h1 initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1, }} className=' text-3xl md:text-5xl font-bold'>Property management, revenue collection and billing under one roof.</motion.h1>
                     <motion.p initial={{ x: 500 }} animate={{ x: 0 }} transition={{ duration: 0.5 }} className='md:text-xl'>Grow recurring revenue, and reduce costs with a single unified platform for payments, subscriptions, invoicing, accounting, tax, and more.</motion.p>
                     <div className="flex flex-col md:flex-row md:justify-between gap-2 md:gap-4">
