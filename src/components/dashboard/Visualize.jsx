73834e135030ff40d9a929be8dd90b9e9602461a
Pacheko <patrickoluochex@gmail.com>
2025-04-05 12:03:10 +0300

    add charts in dashboard

--- src/components/dashboard/Visualize.jsx
+++ src/components/dashboard/Visualize.jsx
@@ -1,6 +1,23 @@
 import React from 'react'
-import { LineChart, Line, CartesianGrid, XAxis, YAxis,Tooltip  } from 'recharts';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer, Legend, PieChart, Pie, } from 'recharts';
 const data = [{ name: 'Page A', uv: 400, pv: 2400, amt: 2400 }];
+const data01 = [
+    { name: 'Group A', value: 400 },
+    { name: 'Group B', value: 300 },
+    { name: 'Group C', value: 300 },
+    { name: 'Group D', value: 200 },
+    { name: 'Group E', value: 278 },
+    { name: 'Group F', value: 189 },
+];
+
+const data02 = [
+    { name: 'Group A', value: 2400 },
+    { name: 'Group B', value: 4567 },
+    { name: 'Group C', value: 1398 },
+    { name: 'Group D', value: 9800 },
+    { name: 'Group E', value: 3908 },
+    { name: 'Group F', value: 4800 },
+];
 
 export default function Visualize() {
     return (
@@ -19,13 +36,12 @@ export default function Visualize() {
                 </div>
                 <div className="flex w-full  md:basis-1/4  bg-gray-100 shadow-lg flex-col py-2">
                     <h1 className='text-xl'>Revenue Collection Per Quarter</h1>
-                    <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-                        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-                        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                        <XAxis dataKey="name" />
-                        <YAxis />
-                        <Tooltip />
-                    </LineChart>
+                    <ResponsiveContainer width="100%" height="100%">
+                        <PieChart width={400} height={400}>
+                            <Pie data={data01} dataKey="value" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" />
+                            <Pie data={data02} dataKey="value" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label />
+                        </PieChart>
+                    </ResponsiveContainer>
 
                 </div>
 
